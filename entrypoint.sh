edbe40afe9c626072adea35f25a5d1c74d9815c7
KINOSHITA Minoru <krtx@users.noreply.github.com>
2018-12-11 17:21:59 +0900

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,4 +2,16 @@
 
 set -e
 
+cat > ~/.netrc << EOF
+machine api.heroku.com
+    login $HEROKU_LOGIN
+    password $HEROKU_API_KEY
+machine git.heroku.com
+    login $HEROKU_LOGIN
+    password $HEROKU_API_KEY
+EOF
+
+# Add heroku.com to the list of known hosts
+ssh-keyscan -H heroku.com >> ~/.ssh/known_hosts
+
 sh -c "heroku $*"
